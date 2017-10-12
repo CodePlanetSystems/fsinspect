6c3fc612d8b19ec69e2ddbd71760a82154d21899
Nicholas Ashley <nicholas.ashley@freetimers.com>
2017-10-12 15:14:01 +0100

    Started to develop the build process

--- make/make.php
+++ make/make.php
@@ -6,8 +6,19 @@ print PHP_EOL;
 $makeScriptPath = getcwd() . '/' . $_SERVER["SCRIPT_FILENAME"];
 
 if (file_exists($makeScriptPath) === false) {
-    print "Aborting build, script path was invalid " . $makeScriptPath . PHP_EOL;
-    print PHP_EOL;
-    exit(1);
+    $makeScriptPath = $_SERVER["SCRIPT_FILENAME"];
+    if (file_exists($makeScriptPath) === false) {
+        print "Aborting build, script path was invalid " . $makeScriptPath . PHP_EOL;
+        print PHP_EOL;
+        exit(1);
+    }
 }
+
+print $makeScriptPath . PHP_EOL;
+print realpath($makeScriptPath) . PHP_EOL;
+
+
+//$makeDirectory = dirname($makeScriptPath);
+//$rootDirectory =
+
 
